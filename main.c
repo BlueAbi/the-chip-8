a00e9cc13422679ab82a767ca47df3fac954c6d6
BlueCorallite <bluecorallite@syn-2600-6c50-4cf0-7d60-0000-0000-0000-12a6.biz6.spectrum.com>
2025-05-28 23:13:31 -0700

    moved window,renderer,texture variables from global to main

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "chip8.h"
 
 Chip8 myChip8;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Texture* texture = NULL;
 
 int main(int argc, char **argv) {
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Texture* texture = NULL;
+
     // Set up render system and input with SDL. Initialize SDL's event handling system
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
         // Outputs error if SDL fails to Initialize
